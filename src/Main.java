b8f5419e0a2cd983513275b981f5e85988d36e71
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-08-03 12:06:12 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by KPS on 8/3/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        char[] chars = {'s','a','l','a','m'};
+        String result = "";
+        for (int i = 0 ; i < chars.length ; i++){
+            result+=chars[i];
+        }
+        System.out.println(result);
+    }
+}
